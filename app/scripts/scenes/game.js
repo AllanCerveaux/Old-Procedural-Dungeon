00cd01be393a68b6a46bc53723d5707f484ab6f7
Callan <cerveauxallanjean@gmail.com>
2019-05-26 18:24:02 +0200

    Inital commit

---
+++ app/scripts/scenes/game.js
@@ -0,0 +1,36 @@
+import Logo from '@/objects/logo';
+
+export default class Game extends Phaser.Scene {
+  /**
+   *  A sample Game scene, displaying the Phaser logo.
+   *
+   *  @extends Phaser.Scene
+   */
+  constructor() {
+    super({key: 'Game'});
+  }
+
+  /**
+   *  Called when a scene is initialized. Method responsible for setting up
+   *  the game objects of the scene.
+   *
+   *  @protected
+   *  @param {object} data Initialization parameters.
+   */
+  create(/* data */) {
+    //  TODO: Replace this content with really cool game code here :)
+    this.logo = this.add.existing(new Logo(this));
+  }
+
+  /**
+   *  Called when a scene is updated. Updates to game logic, physics and game
+   *  objects are handled here.
+   *
+   *  @protected
+   *  @param {number} t Current internal clock time.
+   *  @param {number} dt Time elapsed since last update.
+   */
+  update(/* t, dt */) {
+    this.logo.update();
+  }
+}
