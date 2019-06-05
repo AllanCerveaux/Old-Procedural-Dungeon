76b1f1a63a790ac71e12f072c76c76c1d270af8a
Callan <cerveauxallanjean@gmail.com>
2019-06-05 21:45:19 +0200

    move room and object generator in LevelGenerator

--- app/scripts/scenes/dungeons-scene.js
+++ app/scripts/scenes/dungeons-scene.js
@@ -2,6 +2,7 @@ import Dungeon from '@mikewesthad/dungeon';
 import Player from '../objects/player';
 import TILES from '../objects/tiles-mapping';
 import TilemapVisibility from '../objects/tilemap-visibility';
+import LevelGenerator from '../plugins/level-generator';
 
 export default class DungeonScene extends Phaser.Scene {
   /**
@@ -77,58 +78,13 @@ export default class DungeonScene extends Phaser.Scene {
 
     this.tilemapVisibility = new TilemapVisibility(shadowLayer);
 
-
-    this.dungeon.rooms.forEach(room => {
-      const { x, y, width, height, left, right, top, bottom } = room;
-
-      this.groundLayer.weightedRandomize(x+1, y+1, width-2, height-2, TILES.FLOOR);
-
-
-      this.groundLayer.weightedRandomize(left + 1, top, width - 2, 1, TILES.WALL.TOP);
-      this.groundLayer.weightedRandomize(left + 1, bottom, width - 2, 1, TILES.WALL.TOP);
-      this.groundLayer.weightedRandomize(left, top + 1, 1, height - 2, TILES.WALL.TOP);
-      this.groundLayer.weightedRandomize(right, top + 1, 1, height - 2, TILES.WALL.TOP);
-
-      let doors = room.getDoorLocations();
-      for (let i = 0; i < doors.length; i++) {
-        if (doors[i].y === 0) {
-          this.groundLayer.putTileAt(TILES.DOOR, x + doors[i].x, y + doors[i].y);
-        } else if (doors[i].y === room.height - 1) {
-          this.groundLayer.putTileAt(TILES.DOOR, x + doors[i].x, y + doors[i].y);
-        } else if (doors[i].x === 0) {
-          this.groundLayer.putTileAt(TILES.DOOR, x + doors[i].x, y + doors[i].y);
-        } else if (doors[i].x === room.width - 1) {
-          this.groundLayer.putTileAt(TILES.DOOR, x + doors[i].x, y + doors[i].y);
-        }
-      }
-    });
+    const level = new LevelGenerator(this.dungeon, {groundLayer: this.groundLayer, objectLayer: this.objectLayer});
+    level.init();
 
     const rooms = this.dungeon.rooms.slice();
     const startRoom = rooms.shift();
-    const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
-    const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
 
-    this.objectLayer.putTileAt(TILES.STAIRS, endRoom.centerX, endRoom.centerY);
 
-    otherRooms.forEach(room => {
-      let rand = Math.random();
-      if(rand <= 0.25) {
-        this.objectLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
-      }else if (rand <= 0.5) {
-        const x = Phaser.Math.Between(room.left + 2, room.right - 2);
-        const y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
-        this.objectLayer.weightedRandomize(x, y, 1, 1, TILES.SKULL);
-      }else {
-        if (room.height >= 5) {
-          this.objectLayer.putTileAt(TILES.SKULL, room.centerX - Math.floor(Math.random() * (3 - 1) + 1), room.centerY + Math.floor(Math.random() * (3 - 1) + 1));
-          this.objectLayer.putTilesAt(TILES.BOX, room.centerX - Math.floor(Math.random() * (3 - 1) + 1), room.centerY + Math.floor(Math.random() * (3 - 1) + 1));
-          this.objectLayer.putTilesAt(TILES.BOX, room.centerX + Math.floor(Math.random() * (3 - 1) + 1), room.centerY - Math.floor(Math.random() * (3 - 1) + 1));
-        } else {
-          this.objectLayer.putTilesAt(TILES.BOX, room.centerX - Math.floor(Math.random() * (5 - 1) + 1), room.centerY - Math.floor(Math.random() * (3 - 1) + 1));
-          this.objectLayer.putTilesAt(TILES.BOX, room.centerX + Math.floor(Math.random() * (5 - 1) + 1), room.centerY - Math.floor(Math.random() * (3 - 1) + 1));
-        }
-      }
-    });
 
     this.groundLayer.setCollisionByExclusion([129, 130, 131, 161, 162, 163, 194]);
     this.objectLayer.setCollision([430, 431, 462]);
