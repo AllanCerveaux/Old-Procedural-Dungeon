ec8fccda15045d05bb3bacc8725f8c1a0d2daac9
Callan <cerveauxallanjean@gmail.com>
2019-10-07 20:34:47 +0200

    Revert "No box in door"

--- app/scripts/plugins/level-generator.js
+++ app/scripts/plugins/level-generator.js
@@ -10,7 +10,6 @@ export default class LevelGenerator{
   }
 
   init(){
-    console.log(this.dungeon.tiles);
     this.sword_basic = this.layer.objectLayer.putTileAt(83, 0, 0);
     this.generateDungeonRoom();
     this.generateObject();
@@ -48,107 +47,26 @@ export default class LevelGenerator{
     const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
     const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
 
+    this.layer.objectLayer.putTileAt(TILES.STAIRS, endRoom.centerX, endRoom.centerY);
+
     otherRooms.forEach(room => {
       let rand = Math.random();
-      let x;
-      let y;
-
       if(rand <= 0.25) {
         this.layer.objectLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
       }else if (rand <= 0.5) {
-        x = Phaser.Math.Between(room.left + 2, room.right - 2);
-        y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
+        const x = Phaser.Math.Between(room.left + 2, room.right - 2);
+        const y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
         this.layer.objectLayer.weightedRandomize(x, y, 1, 1, TILES.SKULL);
       }else {
         if (room.height >= 5) {
-
-          do {
-              x = room.centerX - this.randXY(3);
-              y = room.centerY + this.randXY(3);
-          } while( this.coords2neighbours(x,y).indexOf(3) != -1 );
-          this.layer.objectLayer.putTileAt(TILES.SKULL, x, y);
-          
-          do {
-              x = room.centerX - this.randXY(3);
-              y = room.centerY + this.randXY(3);
-          } while( this.coords2neighbours(x,y).indexOf(3) != -1 );
-          this.layer.objectLayer.putTilesAt(TILES.BOX, x, y);
-                    
-          do {
-              x = room.centerX + this.randXY(3);
-              y = room.centerY - this.randXY(3);
-          } while( this.coords2neighbours(x,y).indexOf(3) != -1 );
-          this.layer.objectLayer.putTilesAt(TILES.BOX, x, y);
-          
+          this.layer.objectLayer.putTileAt(TILES.SKULL, room.centerX - Math.floor(Math.random() * (3 - 1) + 1), room.centerY + Math.floor(Math.random() * (3 - 1) + 1));
+          this.layer.objectLayer.putTilesAt(TILES.BOX, room.centerX - Math.floor(Math.random() * (3 - 1) + 1), room.centerY + Math.floor(Math.random() * (3 - 1) + 1));
+          this.layer.objectLayer.putTilesAt(TILES.BOX, room.centerX + Math.floor(Math.random() * (3 - 1) + 1), room.centerY - Math.floor(Math.random() * (3 - 1) + 1));
         } else {
-
-          do {
-            x = room.centerX - this.randXY(5);
-            y = room.centerY - this.randXY(3);
-          } while( this.coords2neighbours(x,y).indexOf(3) != -1 );
-          this.layer.objectLayer.putTilesAt(TILES.BOX, x, y);
-
-          do {
-            x = room.centerX + this.randXY(5);
-            y = room.centerY - this.randXY(3);
-          } while( this.coords2neighbours(x,y).indexOf(3) != -1 );
-          this.layer.objectLayer.putTilesAt(TILES.BOX, x, y);
-          
+          this.layer.objectLayer.putTilesAt(TILES.BOX, room.centerX - Math.floor(Math.random() * (5 - 1) + 1), room.centerY - Math.floor(Math.random() * (3 - 1) + 1));
+          this.layer.objectLayer.putTilesAt(TILES.BOX, room.centerX + Math.floor(Math.random() * (5 - 1) + 1), room.centerY - Math.floor(Math.random() * (3 - 1) + 1));
         }
       }
     });
   }
-
-
-  randXY(v){
-    return Math.floor(Math.random() * (v - 1) + 1);
-  }
-
-  /*
-    Converts an [x,y] coordinate to a tile number...
-  */
-  coords2num(x,y){
-    if( ! this.dungeon.tiles.length)
-    {
-      return;
-    }
-
-    let width = this.dungeon.tiles.length;
-    let height = this.dungeon.tiles[0].length;
-
-    return y*height + x;
-  }
-
-
-  /*
-    Returns the type of tile at the given coordinates :
-     0 = nothing
-     1 = wall
-     2 = ground
-     3 = door
-  */
-  coords2type(x,y){
-    return this.dungeon.tiles[x][y]; 
-  }
-
-
-  /*
-    Get the list of neighbour tiles
-    => Caveat : what about out of bounds ??
-  */
-  coords2neighbours(x,y){
-    return [
-      this.dungeon.tiles[x-1][y-1],
-      this.dungeon.tiles[x  ][y-1],
-      this.dungeon.tiles[x+1][y-1],
-      this.dungeon.tiles[x-1][y  ],
-      this.dungeon.tiles[x  ][y  ],
-      this.dungeon.tiles[x+1][y  ],
-      this.dungeon.tiles[x-1][y+1],
-      this.dungeon.tiles[x  ][y+1],
-      this.dungeon.tiles[x+1][y+1]
-    ];
-
-  }
-
 }
